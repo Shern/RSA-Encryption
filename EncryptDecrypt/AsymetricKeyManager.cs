6d075e5cbc6ee31f0348bc67df02131d1cda75b5
Mathieu Fortin-Boulay <mathieufortinb@outlook.com>
2015-03-09 23:09:26 -0400

    First commit of code

---
+++ EncryptDecrypt/AsymetricKeyManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncryptDecrypt
+{
+    class AsymetricKeyManager
+    {
+        public static void SaveKeyInContainer(string containerName)
+        {
+            var cp = new CspParameters {KeyContainerName = containerName};
+            var rsaKeys = new RSACryptoServiceProvider(cp);
+        }
+
+        public static RSACryptoServiceProvider GetKeyFromContainer(string containerName)
+        {
+            var cp = new CspParameters { KeyContainerName = containerName };
+            return new RSACryptoServiceProvider(cp);
+        }
+
+        public static void DeleteKeyFromContainer(string containerName)
+        {
+            var cp = new CspParameters { KeyContainerName = containerName };
+            var rsaKeys = new RSACryptoServiceProvider(cp) {PersistKeyInCsp = false};
+            rsaKeys.Clear();
+        }
+    }
+}
---
+++ EncryptDecrypt/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Security.Cryptography;
+using System.Windows;
+
+namespace EncryptDecrypt
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void GenerateButton_Click(object sender, RoutedEventArgs e)
+        {
+            var rsaKey = new RSACryptoServiceProvider() {PersistKeyInCsp = false};
+            //var keyPublicXml =  new XDocument(rsaKey.ToXmlString(false));
+            KeySizeTextBlock.Text = rsaKey.KeySize.ToString();
+            PublicKeyGenTextBox.Text = rsaKey.ToXmlString(false);
+            PrivateKeyGenTextBox.Text = rsaKey.ToXmlString(true);
+        }
+    }
+}
